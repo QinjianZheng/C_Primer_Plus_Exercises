03212a78a8e0541d93973890ab1322c9689779bc
bitzhengqj@163.com <qinjian.zheng@student.unsw.edu.au>
2021-02-11 23:49:32 +0800

    update: statement for del vscode debug outputs

--- createMakefile.c
+++ createMakefile.c
@@ -15,14 +15,15 @@ int main(int argc, char const *argv[])
         fprintf(f, "ex%d: ex%d.o\n\t$(CC) -o $@ $^\n\n", i, i);
     }
     FOR_EACH {
-        fprintf(f, "ex%d.o: ex%d.c\n\t$(CC) $(CCFLAGS) -c $^ -o $@\n\n", i, i);
+        fprintf(f, "ex%d.o: ex%d.c\n\t$(CC) $(CCFLAGS) -c $^\n\n", i, i);
     }
-    fprintf(f, "clean:\n\trm -rf *.o ");
+    fprintf(f, "clean:\n\trm -rf *.o *.dSYM");
     FOR_EACH {
         fprintf(f, "ex%d ", i);
     }
     fprintf(f, "\n\n");
-    fprintf(f, "create:\n\tfor ((i=1;i<=%d;i++)); do touch ex$$i.c; done\n", atoi(argv[2]));
+    fprintf(f, "create:\n\tfor ((i=1;i<=%d;i++)); do touch ex$$i.c; \
+        echo \\/\\* $$i\\*\\/ \\\\n\\\\n#include \\<stdio.h\\> > ex$$i.c;done\n", atoi(argv[2]));
 
     return 0;
 }
